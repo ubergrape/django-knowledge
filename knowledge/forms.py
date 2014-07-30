43dee909be6c873171069611d4c6b3cd26b0ea02
Will Schneider <wws9vh@virginia.edu>
2014-07-30 10:22:53 -0400

    Changed the static_url import

--- knowledge/forms.py
+++ knowledge/forms.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 from knowledge import settings
 from knowledge.models import Question, Response
 from knowledge.widgets import CustomRelatedFieldWidgetWrapper
-from settings import STATIC_URL
+import settings
 
 OPTIONAL_FIELDS = ['alert', 'phone_number', 'categories']
 
