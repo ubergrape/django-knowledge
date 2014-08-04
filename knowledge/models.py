a5694fc24c0802812cc4bdb9f7fa78c25a351539
Will Schneider <wws9vh@virginia.edu>
2014-08-04 12:15:56 -0400

    Set user as a required field to fix bug with questions ruining our database

--- knowledge/models.py
+++ knowledge/models.py
@@ -47,7 +47,7 @@ class KnowledgeBase(models.Model):
     added = models.DateTimeField(auto_now_add=True)
     lastchanged = models.DateTimeField(auto_now=True)
 
-    user = models.ForeignKey('auth.User' if django.VERSION < (1, 5, 0) else django_settings.AUTH_USER_MODEL, blank=True,
+    user = models.ForeignKey('auth.User' if django.VERSION < (1, 5, 0) else django_settings.AUTH_USER_MODEL, blank=False,
                              null=True, db_index=True)
     alert = models.BooleanField(default=settings.ALERTS,
         verbose_name=_('Alert'),
